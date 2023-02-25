3cfde58d8a52bbaa734d92310f132e5241dc9aba
sagayda <sagayda27@gmail.com>
2023-02-25 13:51:03 +0200

    MatrixHandler
    MatrixHendler class added

---
+++ DM_Lab1/MatrixHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_Lab2
+{
+    public static class MatrixHandler
+    {
+        public static void PrintMatrix(float[,] matrix)
+        {
+            Console.WriteLine($"{matrix.GetLength(0)} * {matrix.GetLength(1)}\n");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public static float[,] GetMatrixFromFile(string path) 
+        {
+            List<string> tempList = new List<string>();
+
+            tempList = File.ReadAllLines(path).ToList();
+
+            int yLenght = tempList[0].Split(" ").Length;
+            int xLength = tempList.Count;
+
+            float[,] matrix = new float[xLength, yLenght];
+
+            for (int i = 0; i < xLength; i++)
+            {
+                var temp = tempList[i].Split(" ");
+                for (int j = 0; j < yLenght; j++)
+                {
+                    if (temp.Length > yLenght)
+                    {
+                        Console.WriteLine("Bad matrix");
+                        break;
+                    }
+                    matrix[i, j] = float.Parse(temp[j]);
+                }
+            }
+
+            return matrix;
+        }
+
+    }
+}
--- DM_Lab1/Program.cs
+++ DM_Lab1/Program.cs
@@ -1,19 +1,7 @@
-﻿namespace DM_Lab1
+﻿namespace DM_Lab2
 {
     internal class Program
     {
-        public static void PrintMatrix(float[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Console.Write($"{matrix[i, j]}, ");
-                }
-                Console.WriteLine();
-            }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Set path to folder with matrices: (system_matrix.txt and right_vector.txt)");
@@ -22,30 +10,10 @@
             string mainMatrixPath = path + @"\system_matrix.txt";
             string vectorPath = path + @"\right_vector.txt";
 
-            List<string> tempList = new List<string>();
+            float[,] mainMatrix = MatrixHandler.GetMatrixFromFile(mainMatrixPath);
 
-            tempList = File.ReadAllLines(mainMatrixPath).ToList();
+            MatrixHandler.PrintMatrix(mainMatrix);
 
-            int xLenght = tempList[0].Split(" ").Length;
-            int yLength = tempList.Count;
-
-            float[,] mainMatrix = new float[yLength,xLenght];
-
-            Console.WriteLine($"Matrix size: {xLenght} * {yLength}\n");
-
-            for (int i = 0; i < yLength; i++)
-            {
-                var temp = tempList[i].Split(" ");
-                for (int j = 0; j < xLenght; j++)
-                {
-                    if (temp.Length > xLenght)
-                    {
-                        Console.WriteLine("Bad matrix");
-                        break;
-                    }
-                    mainMatrix[i,j] = float.Parse(temp[j]);
-                }
-            }
         }
     }
 }
